efee56a114f6c023ac36221a37934836c0b1b36a
FIakers <asainsardos@gmail.com>
2025-10-15 16:19:18 +0200

    ex00

--- ex00/ipssi_strncmp.c
+++ ex00/ipssi_strncmp.c
@@ -1,1 +1,19 @@
 int	ipssi_strncmp(char *s1, char *s2, unsigned int n)
+{
+	int				diff;
+	unsigned int	i;
+
+	i = 0;
+	diff = 0;
+	if (n < 1)
+		return (0);
+	else
+	{
+		while (i < n)
+		{
+			diff += ((s1[i]) - (s2[i]));
+			i++;
+		}
+		return (diff);
+	}
+}
