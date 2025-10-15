98411ac9c59953fb9ac1463b702840bf93212cbe
FIakers <asainsardos@gmail.com>
2025-10-15 17:40:57 +0200

    ex02 Correction

--- ex02/ipssi_atoi.c
+++ ex02/ipssi_atoi.c
@@ -7,11 +7,12 @@ int	ipssi_atoi(char *str)
 	i = 2;
 	sign = 0;
 	result = 0;
-	while (str[i] == '+' || str[i] == ' ')
+	while (str[i] == ' ')
 		i++;
-	while (str[i] == '-')
+	while (str[i] == '-' || str[i] == '+')
 	{
-		sign += 1;
+		if (str[i] == '-')
+			sign += 1;
 		i++;
 	}
 	while (str[i] >= '0' && str[i] <= '9')
